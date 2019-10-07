66d737f462ec15bfca353a63933b9d42398c2c9f
RafaSalem <40559636+RafaCritik@users.noreply.github.com>
2019-10-07 15:35:22 +0200

    Commit Lunes7-1530

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/observable';
+import { of } from 'rxjs/Observable/of';
+
 import { User } from '../components/models/User';
 @Injectable({
   providedIn: 'root'
   
 })
-export class DataService {
+export class UserService {
 
   users: User[];
+  data: Observable<any>;
 
   constructor() { 
     this.users = [
@@ -36,8 +40,28 @@ export class DataService {
       }
     ];
   }
-  getUsers(): User[] {
-    return this.users;
+
+  getData() {
+    this.data = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(1);
+      }, 1000);
+      setTimeout(() => {
+        observer.next(2);
+      }, 2000);
+      setTimeout(() => {
+        observer.next(3);
+      }, 3000);
+      setTimeout(() => {
+        observer.next(4);
+      }, 4000);
+    });
+
+    return this.data
+  }
+
+  getUsers(): Observable<User[]> {
+    return of(this.users);
   }
 
   addUser(user: User) {
